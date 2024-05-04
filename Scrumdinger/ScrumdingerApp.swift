0e630eea1ddbd6ab29b493e63fc3ae71dcee19ca
Karim Ossama Elmasry <karimelmasry42@icloud.com>
2024-05-04 14:01:12 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Karim Elmasry on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
